cb3133ba290912d39d89fc5ae0d6fa74e21b935c
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-26 20:12:52 +0000

    create the NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/burger.jpg";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="150" />
+        </Navbar.Brand>
+        <Navbar.Text>
+            <a href="#login">Mince 'n' Patties</a>
+        </Navbar.Text>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i class="fa-solid fa-burger"></i> Home
+            </Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-utensils"></i> Login
+            </Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-drumstick-bite"></i> Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
