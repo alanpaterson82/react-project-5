67ebaf148f3eda3d72cd41aebc313101fb754bbe
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-26 21:45:26 +0000

    routing in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="styles.App">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/login" render={() => <h1>Login</h1>} />
+          <Route exact path="/register" render={() => <h1>Register</h1>} />
+          <Route render={() => <p>There's Nothing Here!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,45 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/burger.jpg";
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="150" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="100" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Text>
             <a href="#login">Mince 'n' Patties</a>
         </Navbar.Text>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i class="fa-solid fa-burger"></i> Home
-            </Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-utensils"></i> Login
-            </Nav.Link>
-            <Nav.Link>
-              <i class="fa-solid fa-drumstick-bite"></i> Register
-            </Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-burger"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/login"
+            >
+              <i className="fa-solid fa-utensils"></i>Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-drumstick-bite"></i>Register
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
