961d3e4c9f3b381fdae8b3f49baf04a1f659950e
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-27 16:16:57 +0000

    fix log out bug and amend navbar for log in/out

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://django-rest-ap-9a62d525c1f0.herokuapp.com/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,39 +3,91 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/burger.jpg";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>
-  const loggedOutIcons = <><NavLink
-  className={styles.NavLink}
-  activeClassName={styles.Active}
-  to="/signin"
->
-  <i className="fa-solid fa-utensils"></i>Sign in
-</NavLink>
-<NavLink
-  to="/signup"
-  className={styles.NavLink}
-  activeClassName={styles.Active}
->
-  <i className="fa-solid fa-drumstick-bite"></i>Sign up
-</NavLink>
-</>
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="fa-solid fa-pen-clip"></i>Add post
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fa-solid fa-list-ul"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fa-solid fa-pepper-hot"></i>Hot Stuff
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fa-solid fa-utensils"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fa-solid fa-drumstick-bite"></i>Sign up
+      </NavLink>
+    </>
+  );
 
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
-            <img src={logo} alt="logo" height="150" />
+            <img src={logo} alt="logo" height="125" />
           </Navbar.Brand>
+          <h4>Mince 'n' Patties</h4>
         </NavLink>
-        <Navbar.Text>
-            <a href="#login">Mince 'n' Patties</a>
-        </Navbar.Text>
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -47,6 +99,7 @@ const NavBar = () => {
             >
               <i className="fa-solid fa-burger"></i>Home
             </NavLink>
+
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
