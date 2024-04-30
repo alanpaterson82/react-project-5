bb480b4127b6bb8997ff77dda0cd4c7832e6694a
alanpaterson82 <alan_paterson82@hotmail.com>
2024-04-30 17:15:08 +0000

    CORS

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Avatar.module.css";
 
 const Avatar = ({ src, height = 45, text }) => {
-  const imageUrl = src.includes('default_profile_izly1f') ? 'https://res.cloudinary.com/dmnol3krk/image/upload/v1711113312/media/images/default_profile_izly1f.jpg': src;
+  const imageUrl = src?.includes('default_profile_izly1f') ? 'https://res.cloudinary.com/dmnol3krk/image/upload/v1711113312/media/images/default_profile_izly1f.jpg': src;
   return (
     <span>
       <img
